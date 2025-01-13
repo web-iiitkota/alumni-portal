f7cd09f5a38c120f893570e34c0939e5699c5e80
AlumniIIITKota <alumni@iiitkota.ac.in>
2025-01-13 17:30:16 +0530

    Update carouselHome.jsx

--- client/src/components/carouselHome.jsx
+++ client/src/components/carouselHome.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import "keen-slider/keen-slider.min.css";
-import C2 from "../assets/2.webp";
-import C3 from "../assets/3.webp";
-import C1 from "../assets/1.webp";
+import Disha from "../../public/assets/1.png";
+import LinkedinLinkup from "../../public/assets/3.png";
+import ResearchTrajectory from "../../public/assets/4.png";
+import AlumniInsights from "../../public/assets/2.png";
 import { useKeenSlider } from "keen-slider/react";
 
 const carouselHome = () => {
@@ -38,13 +39,16 @@ const carouselHome = () => {
         className="keen-slider lg:h-[78vh] md:h-[65vh] sm:h-[55vh] h-[30vh] mt-[8.375rem] max-w-980:mt-[90px] max-w-492:mt-[58px]"
       >
         <div className="keen-slider__slide flex justify-center items-center">
-          <img src={C1} alt="" className=" object-fill w-full h-full" />
+          <img src={Disha} alt="" className=" object-fill w-full h-full" />
         </div>
         <div className="keen-slider__slide flex justify-center items-center">
-          <img src={C2} alt="" className="object-fill w-full h-full" />
+          <img src={LinkedinLinkup} alt="" className="object-fill w-full h-full" />
         </div>
         <div className="keen-slider__slide flex justify-center items-center">
-          <img src={C3} alt="" className="object-fill w-full h-full" />
+          <img src={ResearchTrajectory} alt="" className="object-fill w-full h-full" />
+        </div>
+        <div className="keen-slider__slide flex justify-center items-center">
+          <img src={AlumniInsights} alt="" className="object-fill w-full h-full" />
         </div>
       </div>
     </div>
