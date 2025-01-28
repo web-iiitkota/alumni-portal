060a63fb8299d20fbcad898b2770c47927426129
AlumniIIITKota <alumni@iiitkota.ac.in>
2025-01-28 22:37:09 +0530

    Update NewsCard.jsx

--- client/src/components/NewsCard.jsx
+++ client/src/components/NewsCard.jsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, Typography, Link, Box } from '@mui/material';
 
-const NewsCard = ({ id, title, content, referenceLink, postedOn }) => {
+const NewsCard = ({ id, title, content, postedOn }) => {
   return (
     <Card id={id} sx={{ width: {xl: 1200, lg: 1000, md: 800, sm: 600, xs: 350}, boxShadow: 3, borderRadius: 2, transition: "transform 0.3s, box-shadow 0.3s", "&:hover": { transform: "translateY(-5px)", boxShadow: 6 } }}>
       <CardContent>
@@ -11,9 +11,6 @@ const NewsCard = ({ id, title, content, referenceLink, postedOn }) => {
           <div dangerouslySetInnerHTML={{ __html: content }} />
         </Typography>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 2 }}>
-          <Link href={referenceLink} target="_blank" rel="noopener" sx={{ color: '#007BFF', "&:hover": { color: '#0056b3' } }}>
-            Reference
-          </Link>
           <Typography variant="body2" color="text.secondary">
             Posted on: {postedOn}
           </Typography>
